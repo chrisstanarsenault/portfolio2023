c4bea3b5bc79c019cf1321228d09c54bf9d4677a
Chris Arsenault <chrisstanarsenault@gmail.com>
2022-12-29 16:48:17 -0500

    feat: :sparkles: added basic intro section and updated eslint formating

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-gradient-to-b from-[#0f2027] to-[#203a43] w-full h-screen">
+    <main class="text-white max-w-layout min-h-screen mx-auto w-full px-6 md:px-24 lg:px-40">
+      <!-- Intro -->
+      <section class="space-y-6 min-h-screen w-full mx-auto flex flex-col justify-center">
+        <p>Hello wor.... I mean, oh hey! I'm</p>
+
+        <div class="space-y-2">
+          <h1 class="text-4xl md:text-6xl lg:text-7xl">Chris Arsenault.</h1>
+          <h2 class="text-4xl md:text-6xl lg:text-7xl text-gray-300">I'm a frontend developer.</h2>
+        </div>
+
+        <p class="max-w-2xl">I'm a software engineer specializing in building (and occasionally designing) exceptional
+          digital experiences. Currently, I'm focused on building accessible, human-centered products at Upstatement.
+        </p>
+      </section>
+    </main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script setup lang="ts">
+const hello = 'hello'
+</script>
